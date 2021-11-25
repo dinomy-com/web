7f5c9823ee41e76393d1c187ed550f062bce5a03
dinomy-com <95028944+dinomy-com@users.noreply.github.com>
2021-11-25 17:33:58 +0400

    Add files via upload

---
+++ pass9.js
@@ -0,0 +1,9 @@
+function pass9() {
+    var length = 9,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
